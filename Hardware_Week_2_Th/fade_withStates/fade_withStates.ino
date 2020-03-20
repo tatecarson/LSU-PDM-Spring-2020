424e6eca9a95e64820473853b483393d9af492dd
tatecarson <tatecarson@gmail.com>
2020-03-20 10:37:51 -0500

    updated fade with states

--- Hardware_Week_2_Th/fade_withStates/fade_withStates.ino
+++ Hardware_Week_2_Th/fade_withStates/fade_withStates.ino
@@ -3,10 +3,11 @@ int led = 9;
 int brightness = 200;
 int fadeAmount = 5;
 int pot;
-int button1 = 0;
-int button2 = 0;
-int button1State = 0;
-int button2State = 0;
+
+int ledModeToggle = 0;
+int ledOnToggle = 0;
+int ledModeToggleState = 0;
+int ledOnToggleState = 0;
 
 // States allow us to turn the button from a momentary button to a toggle button
 // now we can press the button once to switch between two states
@@ -16,35 +17,30 @@ void setup()
   // because we're using a digital pin we have
   // to set the mode to input or output
   pinMode(led, OUTPUT);
-  pinMode(2, INPUT);
-  pinMode(4, INPUT);
   Serial.begin(9600);
 }
 
 void loop()
 {
   // toggle between fading and using pot
-  button1 = digitalRead(2);
+  ledModeToggle = digitalRead(2);
 
-  if (button1)
-  {
+  if (ledModeToggle) {
     // this only kind of works
     // see how it polls back and forth between states way too quickly to be useful
-    button1State = !button1State;
+    ledModeToggleState = !ledModeToggleState;
   }
 
   // turn everything on or off
-  button2 = digitalRead(4);
+  ledOnToggle = digitalRead(4);
 
-  if (button2)
-  {
-    button2State = !button2State;
+  if (ledOnToggle) {
+    ledOnToggleState = !ledOnToggleState;
   }
 
-  //  Serial.println(button2State);
+  Serial.println(ledModeToggleState);
 
-  if (button1State && button2State)
-  {
+  if (ledModeToggleState && ledOnToggleState) {
     // Control fade with pot
     ///////////////////////
 
@@ -57,23 +53,19 @@ void loop()
     // set the brightness of pin 9 with pot value
     analogWrite(led, brightness);
   }
-  else if (button1State == LOW && button2State)
-  {
+  else if (!ledModeToggleState && ledOnToggleState) {
     // autofade with button press
     /////////////////////////////
 
     // increase brightness by fadeAmount
     brightness = brightness + fadeAmount;
 
-    if (brightness <= 0 || brightness >= 255)
-    {
+    if (brightness <= 0 || brightness >= 255) {
       // toggle increase and decrease
       fadeAmount = -fadeAmount;
     }
     analogWrite(led, brightness);
-  }
-  else if (button2State == LOW)
-  {
+  } else {
     // if neither button pressed turn LED off
     analogWrite(led, 0);
   }
